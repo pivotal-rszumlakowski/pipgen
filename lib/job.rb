c2574adae2a5cb7245dca4b29268195e868428e4
Rob Szumlakowski <rszumlakowski@pivotal.io>
2017-08-25 09:40:20 -0400

    Use symbols for keys at the top level of job definition hashes

--- lib/job.rb
+++ lib/job.rb
@@ -7,22 +7,21 @@ class Job
 
 	def initialize(job_hash)
 
-		@job_hash = job_hash
+		@job_hash = job_hash.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo} # Converts keys in hash to symbols
 		@name = ""
 
-		raise "Empty job definition" if job_hash.empty?
+		raise "Empty job definition" if @job_hash.empty?
 
-		@name = job_hash["name"] if job_hash.key? "name"
-		@name = job_hash[:name] if job_hash.key? :name
+		@name = @job_hash[:name]
 		@plan_gets = []
 		@depends_on = []
 
-		return unless job_hash.key? "plan"
-		return if job_hash["plan"].nil? or job_hash["plan"].empty?
+		return unless @job_hash.key? :plan
+		return if @job_hash[:plan].nil? or @job_hash[:plan].empty?
 
 		all_get_items = []
 		all_get_items.concat(plan_aggregate_items())
-		all_get_items.concat(get_items(job_hash["plan"]))
+		all_get_items.concat(get_items(@job_hash[:plan]))
 
 		all_get_items.each { |get_item|
 			@plan_gets << get_item
@@ -36,7 +35,7 @@ class Job
 	private
 
 	def plan_aggregate_items
-		job_hash["plan"].select{|p| p.key?("aggregate")}.collect{|p| p["aggregate"]}.flatten
+		@job_hash[:plan].select{|p| p.key?("aggregate")}.collect{|p| p["aggregate"]}.flatten
 	end
 
 	def get_items(plan)
--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -3,8 +3,10 @@ require 'yaml'
 require 'job'
 
 FactoryGirl.define do
+
 	factory :job do
 		name "awesome_job"
 		initialize_with {new(attributes)}
 	end
+
 end
